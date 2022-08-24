3dd486d7b69028c9a3b30381dae129d99eae5035
Matt Weiss <mweiss427@gmail.com>
2022-08-24 15:59:39 -0500

    good try, not ready yet, going back to school.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import NextUp from './components/NextUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Books </h1>
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 20 }}>
+        <div> Up Next
+        <NextUp/>
+        </div>
+
+        <div>Book</div>
+        <div>Author</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/NextUp.js
@@ -0,0 +1,19 @@
+import React from'react'; 
+  
+const NextUp=(book)=> 
+{
+    console.log(book);
+return (
+    
+    <h1>Book: Hi</h1>
+   );
+}
+  
+const functionExample=()=>
+{
+    return (
+    <NextUp/>
+   );
+}
+  
+export default NextUp;
